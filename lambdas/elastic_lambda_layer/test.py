2c3876e848064b6b88bfab7d0dfe82e54c7326fa
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-21 16:29:21 +0100

    Wooohoooo

--- lambdas/elastic_lambda_layer/test.py
+++ lambdas/elastic_lambda_layer/test.py
@@ -1,5 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('./python'))
+from elasticsearch import Elasticsearch
+
 def test():
-    assert True
+    assert Elasticsearch is not None
 
 if __name__ == '__main__':
     test()
