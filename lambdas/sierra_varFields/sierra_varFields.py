4a2813466a7298e73745aec9adfd59b56fa0b3ad
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-21 16:24:32 +0100

    Bloop

--- lambdas/sierra_varFields/sierra_varFields.py
+++ lambdas/sierra_varFields/sierra_varFields.py
@@ -9,9 +9,9 @@ import botocore
 
 # This is used for local testing and will not get used by lambdas.
 # It doesn't matter as adding an empty path never hurt anyone.
-sys.path.insert(0, os.path.abspath('../../elastic_lambda_layer/python'))
-from elasticsearch import Elasticsearch
+sys.path.insert(0, os.path.abspath('../elastic_lambda_layer/python'))
 from elasticsearch import helpers
+from elasticsearch import Elasticsearch
 
 s3_client = boto3.client('s3')
 
@@ -19,6 +19,8 @@ s3_client = boto3.client('s3')
 # * Error handling
 # * Logging
 # * Move bits into layers i.e. Elasticsearch
+
+
 def get_es_credentials():
     secret_name = "prod/Elasticsearch/ReportingCredentials"
     region_name = "eu-west-1"
@@ -35,63 +37,63 @@ def get_es_credentials():
     secret = get_secret_value_response['SecretString']
     return json.loads(secret)
 
+
 def flatten_varField(varField):
     full_content_delimeter = ' '
-    label = full_content_delimeter.join([subfield['content'] for subfield in varField['subfields']])
-    flattened_subfields = {subfield['tag']: subfield['content'] for subfield in varField['subfields']}
+    label = full_content_delimeter.join(
+        [subfield['content'] for subfield in varField['subfields']])
+    flattened_subfields = {subfield['tag']: subfield['content']
+                           for subfield in varField['subfields']}
     return {
         'label': label,
         **flattened_subfields
     }
 
+
 index = 'reporting_sierra_varfields'
+
+def transform(data):
+    varFields = data['varFields']
+    material_type = data['materialType']['code'].strip()
+
+    if len(varFields) > 0:
+        flattened_varFields = {varField['marcTag']: flatten_varField(varField) for varField in varFields if 'subfields' in varField}
+        return {
+            'id': data['id'],
+            'material_type': material_type,
+            'varfields': flattened_varFields
+        }
+
+
+
 def main(event, context):
     message_json_string = event['Records'][0]['Sns']['Message']
     message = json.loads(message_json_string)
     location = message['location']
-    response = s3_client.get_object(Bucket=location['namespace'], Key=location['key'])
+    response = s3_client.get_object(
+        Bucket=location['namespace'], Key=location['key'])
     body = response['Body'].read().decode('utf-8')
     s3Json = json.loads(body)
     maybe_bib_record = s3Json['maybeBibRecord']
     if (maybe_bib_record):
         data = json.loads(maybe_bib_record['data'])
-        material_type = data['materialType']['code'].strip()
+        es_doc = transform(data)
 
-        varFields = data['varFields']
-        id = data['id']
-
-        if len(varFields) > 1:
+        if es_doc:
             es_credentials = get_es_credentials()
             es = Elasticsearch(
                 es_credentials['url'],
-                http_auth=(es_credentials['username'], es_credentials['password']),
+                http_auth=(es_credentials['username'],
+                           es_credentials['password']),
                 use_ssl=True,
                 ca_certs=certifi.where(),
             )
-            """
-            We're looking for a format of
-            {
-                "id": "129038",
-                "varfields": {
-                    "260": {
-                        "label": "London : Faber and Faber limited, [1938]",
-                        "a": "London : ",
-                        "b": "Faber and Faber limited,",
-                        "c": "[1938]"
-                    }
-                }
-            }
-            We then do updates, which will eventually lead us to
-            """
-            flattened_varFields = {varField['marcTag']: flatten_varField(varField) for varField in varFields if 'subfields' in varField}
+
             res = es.update(
                 index=index,
-                id=id,
+                id=es_doc['id'],
                 body={
-                    'doc': {
-                        'material_type': material_type,
-                        'varfields': flattened_varFields
-                    },
+                    'doc': es_doc,
                     'doc_as_upsert': True
                 },
                 doc_type='doc'
@@ -100,4 +102,3 @@ def main(event, context):
             print(res)
         else:
             print('No varFields to index for record {}'.format(id))
-
--- lambdas/sierra_varFields/test.py
+++ lambdas/sierra_varFields/test.py
@@ -1,5 +1,47 @@
+from sierra_varFields import transform
+
 def test():
-    assert True
+    vhs_data = {
+        'id': '129038',
+        'materialType': {
+            'code': 'a '
+        },
+        'varFields': [
+            {
+                'marcTag': '260',
+                'fieldTag': 'a',
+                'ind1': '1',
+                'ind2': ' ',
+                'subfields': [
+                    {
+                        'tag': 'a',
+                        'content': 'London :'
+                    },
+                    {
+                        'tag': 'b',
+                        'content': 'Faber and Faber limited,'
+                    },
+                    {
+                        'tag': 'c',
+                        'content': '[1938]'
+                    }
+                ]
+            }
+        ]
+    }
+    expected_data = {
+        'id': '129038',
+        'material_type': 'a',
+        'varfields': {
+            '260': {
+                'label': 'London : Faber and Faber limited, [1938]',
+                'a': 'London :',
+                'b': 'Faber and Faber limited,',
+                'c': '[1938]'
+            }
+        }
+    }
+    assert transform(vhs_data) == expected_data
 
 if __name__ == '__main__':
     test()
