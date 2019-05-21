a59630e8ca1dd93d8c2f29ad798ecbd24572be4f
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-21 12:30:56 +0100

    Bloooop

--- actions/test_lambda/entrypoint.sh
+++ actions/test_lambda/entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 LAMBDA_NAME=$1
-LAMBDA_DIR="./lambdas/$1"
+LAMBDA_DIR="./lambdas/$LAMBDA_NAME"
 
 cd $LAMBDA_DIR
   if test -f "./requirements.txt"; then
@@ -10,4 +10,5 @@ cd $LAMBDA_DIR
   fi
 
   python3 ./test.py
+  echo "Finished testing $LAMBDA_NAME"
 cd ../../
