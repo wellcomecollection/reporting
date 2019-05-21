48e1a588e17edaf85c76a3a29c0e2f62d81d0bd7
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-21 12:23:29 +0100

    Blaarp

--- actions/test_lambda/entrypoint.sh
+++ actions/test_lambda/entrypoint.sh
@@ -6,10 +6,10 @@ LAMBDA_DIR="./lambdas/$1"
 
 
 
-pushd $LAMBDA_DIR
+cd $LAMBDA_DIR
   if test -f "./requirements.txt"; then
     pip install -r requirements.txt
   fi
 
   python3 ./test.py
-popd
+cd ../../
