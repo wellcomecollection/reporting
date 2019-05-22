d2869f73cae46f4a896fa16d37a6e035a2c96ff1
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-22 16:05:29 +0100

    add logging for requirements in lambda test dockerfile

--- actions/test_lambda/entrypoint.sh
+++ actions/test_lambda/entrypoint.sh
@@ -5,8 +5,12 @@ LAMBDA_NAME=$1
 LAMBDA_DIR="./lambdas/$LAMBDA_NAME/src"
 
 cd $LAMBDA_DIR
-  if test -f "./requirements.txt"; then
+  if test -f "./requirements.txt"
+  then
+    echo "Installing pip requirements"
     pip3 install -r requirements.txt
+  else
+    echo "No pip requirements"
   fi
 
   pytest .
