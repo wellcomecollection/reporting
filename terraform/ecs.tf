fa52eb89eff541ffc7f1079b50eeefe947e76be5
harrisonpim <harrison.stroma@gmail.com>
2019-11-14 14:01:04 +0000

    add scheduled tasks in tf for precision and NDCG calculations

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -4,16 +4,16 @@ resource "aws_ecs_cluster" "reporting_aggregations_cluster" {
 }
 
 ## Scheduled tasks
-module "scheduled_task" {
+module "scheduled_task_search_relevance_explicit_NDCG" {
   source              = "./modules/scheduled_ecs_task"
-  name                = "aggregate_relevance_metrics"
-  schedule_expression = "cron(0 6 * * ? *)" 
+  name                = "search_relevance_explicit_NDCG"
+  schedule_expression = "cron(0 6 * * ? *)"
   cluster_arn         = "${aws_ecs_cluster.reporting_aggregations_cluster.arn}"
 
   container_definitions = <<EOF
     [{
-        "name": "aggregate_relevance_metrics",
-        "image": "harrisonpim/aggregate_relevance_metrics:latest",
+        "name": "search_relevance_explicit_NDCG",
+        "image": "harrisonpim/search_relevance_explicit_NDCG:latest",
         "cpu": 512,
         "memory": 512,
         "essential": true,
@@ -24,3 +24,28 @@ module "scheduled_task" {
     }]
     EOF
 }
+
+module "scheduled_task_search_relevance_explicit_precision" {
+  source              = "./modules/scheduled_ecs_task"
+  name                = "search_relevance_explicit_precision"
+  schedule_expression = "cron(0 6 * * ? *)"
+  cluster_arn         = "${aws_ecs_cluster.reporting_aggregations_cluster.arn}"
+
+  container_definitions = <<EOF
+    [{
+        "name": "search_relevance_explicit_precision",
+        "image": "harrisonpim/search_relevance_explicit_precision:latest", 
+        "cpu": 512,
+        "memory": 512,
+        "essential": true,
+        "portMappings": [{
+            "containerPort": 80,
+            "hostPort": 80
+        }]
+    }]
+    EOF
+}
+
+# n.b. the images in the container definitions above don't actually exist yet.
+# they still need to be built and pushed to ECR/dockerhub at some point.
+
