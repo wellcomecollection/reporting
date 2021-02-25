b199340bd7553d5ac13bb43792db9a679cdab0f1
Alex Chan <alex@alexwlchan.net>
2021-02-25 13:44:35 +0000

    Index items and holdings as well!

--- lambdas/new_sierra_transformer/src/new_sierra_transformer.py
+++ lambdas/new_sierra_transformer/src/new_sierra_transformer.py
@@ -63,6 +63,74 @@ def add_check_digit(id, *, prefix):
         return prefix + id + str(total % 11)
 
 
+def create_requests_for(*, record_type, record, index_name):
+    index_requests = []
+    delete_by_query_requests = []
+
+    id = add_check_digit(record["id"], prefix=record_type[0])
+
+    indexed_data = {
+        k: v for k, v in record.items() if k not in {"varFields", "fixedFields"}
+    }
+
+    print(f"Indexing data on {id}")
+    index_requests.append((index_name, id, indexed_data))
+
+    varfields = record["varFields"]
+    fixed_fields = record["fixedFields"]
+
+    print(f"Indexing varFields on {id}")
+    for position, vf in enumerate(varfields):
+        vf["_parent"] = {
+            "record_type": record_type,
+            "id": id,
+            "position": position,
+        }
+
+        index_requests.append(("sierra_varfields", f"{id}-{position}", vf))
+
+    delete_by_query_requests.append(
+        (
+            "sierra_varfields",
+            {
+                "query": {
+                    "bool": {
+                        "filter": [
+                            {"term": {"_parent.id": id}},
+                            {"range": {"_parent.position": {"gte": len(varfields)}}},
+                        ]
+                    }
+                }
+            },
+        )
+    )
+
+    print(f"Indexing fixedFields on {id}")
+    for code, ff in fixed_fields.items():
+        ff["_code"] = code
+        ff["_parent"] = {"record_type": record_type, "id": id}
+
+        index_requests.append(("sierra_fixedfields", f"{id}-{code}", ff))
+
+    delete_by_query_requests.append(
+        (
+            "sierra_fixedfields",
+            {
+                "query": {
+                    "bool": {
+                        "filter": [
+                            {"term": {"_parent.id": id}},
+                        ],
+                        "must_not": [{"terms": {"_code": list(fixed_fields.keys())}}],
+                    }
+                }
+            },
+        )
+    )
+
+    return index_requests, delete_by_query_requests
+
+
 def main(event, _):
     credentials = get_es_credentials()
     es_client = httpx.Client(
@@ -77,108 +145,44 @@ def main(event, _):
 
     for payload in get_payloads(event, s3_read_role=s3_read_role):
 
-        # Index the bib record, with the varFields and fixedFields
-        # going to a separate index.
         try:
             bib_data = json.loads(payload["transformable"]["maybeBibRecord"]["data"])
         except KeyError as err:
             print(f"No bib data for {payload['id']}: KeyError / {err}")
         else:
-            indexed_bib_data = {
-                k: v
-                for k, v in bib_data.items()
-                if k not in {"varFields", "fixedFields"}
-            }
-
-            bib_id = add_check_digit(bib_data["id"], prefix="b")
-
-            print(f"Indexing data on {bib_id}")
-            index_requests.append(
-                ({"index": {"_index": "sierra_bibs", "_id": bib_id}}, indexed_bib_data)
+            bib_index_requests, bib_delete_by_query_requests = create_requests_for(
+                record_type="bib", record=bib_data, index_name="sierra_bibs"
             )
 
-            print(f"Indexing varFields on {bib_id}")
-            for position, vf in enumerate(bib_data["varFields"]):
-                vf["_parent"] = {
-                    "record_type": "bib",
-                    "id": bib_id,
-                    "position": position,
-                }
+            index_requests.extend(bib_index_requests)
+            delete_by_query_requests.extend(bib_delete_by_query_requests)
 
-                index_requests.append(
-                    (
-                        {
-                            "index": {
-                                "_index": "sierra_varfields",
-                                "_id": f"{bib_id}-{position}",
-                            }
-                        },
-                        vf,
-                    )
-                )
+        for item_record in payload["transformable"].get("itemRecords", {}).values():
+            item_data = json.loads(item_record["data"])
 
-            delete_by_query_requests.append(
-                (
-                    "sierra_varfields",
-                    {
-                        "query": {
-                            "bool": {
-                                "filter": [
-                                    {"term": {"_parent.id": bib_id}},
-                                    {
-                                        "range": {
-                                            "_parent.position": {
-                                                "gte": len(bib_data["varFields"])
-                                            }
-                                        }
-                                    },
-                                ]
-                            }
-                        }
-                    },
-                )
+            item_index_requests, item_delete_by_query_requests = create_requests_for(
+                record_type="item", record=item_data, index_name="sierra_items"
             )
 
-            print(f"Indexing fixedFields on {bib_id}")
-            for code, fixed_field in bib_data["fixedFields"].items():
-                fixed_field["_code"] = code
-                fixed_field["_parent"] = {"record_type": "bib", "id": bib_id}
+            index_requests.extend(item_index_requests)
+            delete_by_query_requests.extend(item_delete_by_query_requests)
 
-                index_requests.append(
-                    (
-                        {
-                            "index": {
-                                "_index": "sierra_fixedfields",
-                                "_id": f"{bib_id}-{code}",
-                            }
-                        },
-                        fixed_field,
-                    )
-                )
+        for holding_record in (
+            payload["transformable"].get("holdingRecords", {}).values()
+        ):
+            holding_data = json.loads(holding_record["data"])
 
-            delete_by_query_requests.append(
-                (
-                    "sierra_fixedfields",
-                    {
-                        "query": {
-                            "bool": {
-                                "filter": [
-                                    {"term": {"_parent.id": bib_id}},
-                                ],
-                                "must_not": [
-                                    {
-                                        "terms": {
-                                            "_code": list(
-                                                bib_data["fixedFields"].keys()
-                                            )
-                                        }
-                                    }
-                                ],
-                            }
-                        }
-                    },
-                )
+            (
+                holding_index_requests,
+                holding_delete_by_query_requests,
+            ) = create_requests_for(
+                record_type="holdings",
+                record=holding_data,
+                index_name="sierra_holdings",
             )
+
+            index_requests.extend(holding_index_requests)
+            delete_by_query_requests.extend(holding_delete_by_query_requests)
 
         # Now construct a bulk request to send to Elasticsearch.
         # We do a bulk request rather than individual lines to avoid the
@@ -188,8 +192,10 @@ def main(event, _):
 
         print(f"Indexing {len(index_requests)} documents")
 
-        for index, request in index_requests:
-            bulk_request_lines.append(json.dumps(index))
+        for index, id, request in index_requests:
+            bulk_request_lines.append(
+                json.dumps({"index": {"_index": index, "_id": id}})
+            )
             bulk_request_lines.append(json.dumps(request))
 
         # TODO: Look for documents that didn't get indexed correctly
