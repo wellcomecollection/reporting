2d0648e4ba801bad586e1967d98404e99d4182d8
James Gorrie <j.gorrie@wellcome.org>
2021-10-26 13:55:17 +0100

    use _ in querystring field names

--- conversion_analytics/elasticsearch/component_template.ts
+++ conversion_analytics/elasticsearch/component_template.ts
@@ -1,113 +1,118 @@
-import { name as ilmName } from "./ilm"
-import querystringParameters from "./querystringParameters"
+import { name as ilmName } from './ilm'
+import querystringParameters from './querystringParameters'
 
 export const name = 'conversion_component_template'
 
 export const body = {
-  "template": {
-    "settings": {
-      "index.lifecycle.name": ilmName,
-      "analysis": {
-        "analyzer": {
-          "csv_analyzer": {
-            "tokenizer": "csv_tokenizer"
+  template: {
+    settings: {
+      'index.lifecycle.name': ilmName,
+      analysis: {
+        analyzer: {
+          csv_analyzer: {
+            tokenizer: 'csv_tokenizer'
           }
         },
-        "tokenizer": {
-          "csv_tokenizer": {
-            "type": "pattern",
-            "pattern": ","
+        tokenizer: {
+          csv_tokenizer: {
+            type: 'pattern',
+            pattern: ','
           }
         }
       }
     },
-    "mappings": {
-      "dynamic": false,
-      "dynamic_templates": [
+    mappings: {
+      dynamic: false,
+      dynamic_templates: [
         {
-          "properties_params": {
-            "path_match": "properties.*",
-            "mapping": {
-              "type": "text",
-              "fields": {
-                "keyword": {
-                  "type": "keyword"
+          properties_params: {
+            path_match: 'properties.*',
+            mapping: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword'
                 },
-                "csv": {
-                  "type": "text",
-                  "analyzer": "csv_analyzer"
+                csv: {
+                  type: 'text',
+                  analyzer: 'csv_analyzer'
                 },
-                "number": {
-                  "type": "long",
-                  "ignore_malformed": true
+                number: {
+                  type: 'long',
+                  ignore_malformed: true
                 },
-                "wildcard": {
-                  "type": "wildcard"
+                wildcard: {
+                  type: 'wildcard'
                 }
               }
             }
           }
         }
       ],
-      "properties": {
-        "@timestamp": {
-          "type": "date"
+      properties: {
+        '@timestamp': {
+          type: 'date'
         },
-        "anonymousId": {
-          "type": "keyword"
+        anonymousId: {
+          type: 'keyword'
         },
-        "session": {
-          "properties": {
-            "id": {
-              "type": "keyword"
+        session: {
+          properties: {
+            id: {
+              type: 'keyword'
             },
-            "timeout": {
-              "type": "long"
+            timeout: {
+              type: 'long'
             }
           }
         },
-        "type": {
-          "type": "keyword"
+        type: {
+          type: 'keyword'
         },
-        "source": {
-          "type": "keyword",
-          "fields": {
-            "wildcard": {
-              "type": "wildcard"
+        source: {
+          type: 'keyword',
+          fields: {
+            wildcard: {
+              type: 'wildcard'
             }
           }
         },
-        "page": {
-          "properties": {
-            "name": {
-              "type": "keyword"
+        page: {
+          properties: {
+            name: {
+              type: 'keyword'
             },
-            "path": {
-              "type": "keyword"
+            path: {
+              type: 'keyword'
             },
-            "pathname": {
-              "type": "keyword"
+            pathname: {
+              type: 'keyword'
             },
-            "query": {
-              "dynamic": false,
-              "properties": Object.entries(querystringParameters).reduce((acc, [key, type]) => {
-                const typeObj =
-                  type === 'csv' ? { type: 'text', "analyzer": "csv_analyzer" } :
-                  type === 'float' ? { type: 'float', ignore_malformed: true } :
-                  { type }
-                return {
-                  ...acc,
-                  [key]: typeObj
-                }
-              }, {})
+            query: {
+              dynamic: false,
+              properties: Object.entries(querystringParameters).reduce(
+                (acc, [key, type]) => {
+                  const typeObj =
+                    type === 'csv'
+                      ? { type: 'text', analyzer: 'csv_analyzer' }
+                      : type === 'float'
+                        ? { type: 'float', ignore_malformed: true }
+                        : { type }
+                  return {
+                    ...acc,
+                    [key.replace(/\./g, '_')]: typeObj
+                  }
+                },
+                {}
+              )
             }
           }
         },
-        "properties": {
-          "dynamic": true,
-          "properties": {}
+        properties: {
+          dynamic: true,
+          properties: {}
         }
       }
     }
-  },
+  }
 }
